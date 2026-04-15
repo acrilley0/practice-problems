7cc0d984c3e9489986e22636e080531d8ab30445
aidencrilley <acrilley8@gmail.com>
2026-04-15 12:59:36 -0400

    Corrected and simplified previous code

--- gcd-linked-list-COMPLETE/main.cpp
+++ gcd-linked-list-COMPLETE/main.cpp
@@ -1,54 +1,28 @@
 #include <iostream>
-#include <cassert>
-#include <memory>
-#include <vector>
 #include <numeric>
-
-using std::cout;
-using std::endl;
-using std::vector;
 
 struct ListNode {
   int val;
-  std::shared_ptr<ListNode> next;
+  ListNode* next;
   ListNode() : val(0), next(nullptr) {}
   ListNode(int x) : val(x), next(nullptr) {}
   ListNode(int x, ListNode *next) : val(x), next(next) {}
 };
 
-std::shared_ptr<ListNode> createListSP(vector<int> & nums)
-{
-  if (nums.empty()) {
-    cout << "Got an empty vector" << endl;
-    return nullptr;
-  }
-  std::shared_ptr<ListNode> head = std::make_shared<ListNode>(nums[0]);
-  assert(head);
-
-  std::shared_ptr<ListNode> iter = head;
-
-  for (int i = 1; i < nums.size(); i++) {
-    iter->next = std::make_shared<ListNode>(nums[i]);
-    iter = iter->next;
-  }
-
-  return head;
-}
-
 class Solution {
 public:
-  ListNode* insertGreatestCommonDivisors(std::shared_ptr<ListNode> & head_shared)
-  {
-    std::shared_ptr<ListNode> head = head_shared;
+  ListNode* insertGreatestCommonDivisors(ListNode* head) {
+    ListNode* currp = head;
 
-    while (head->next) {
-      int gcd_val = std::gcd(head->val, head->next->val);
-      auto gcd = std::make_shared<ListNode>(gcd_val);
-      gcd->next = head->next;
-      head->next = gcd;
-      head = gcd->next;
+    while (currp->next) {
+      int gcd_val = std::gcd(currp->val, currp->next->val);
+      ListNode* tmp = currp->next;
+      ListNode* gcd_node = new ListNode(gcd_val, tmp);
+      currp->next = gcd_node;
+      currp = currp->next->next;
     }
-    return head_shared.get();
+
+    return head;
   }
 };
 
@@ -56,18 +30,28 @@ int main()
 {
   Solution sol;
 
-  vector<int> case1 = {18,6,10, 3};
-  std::shared_ptr<ListNode> test1 = createListSP(case1);
-  ListNode *gcd1 = sol.insertGreatestCommonDivisors(test1);
+  // 18->6->10->3
+  ListNode l1 = ListNode(18);
+  ListNode l2 = ListNode(6);
+  ListNode l3 = ListNode(10);
+  ListNode l4 = ListNode(3);
 
-  vector<int> case2 = {7};
-  std::shared_ptr<ListNode> test2 = createListSP(case2);
-  ListNode *gcd2 = sol.insertGreatestCommonDivisors(test2);
+  l1.next = &l2;
+  l2.next = &l3;
+  l3.next = &l4;
+  l4.next = nullptr;
 
-  ListNode *head = gcd1;
-  while (head != nullptr) {
-    cout << head->val << " ";
-    head = head->next.get();
+  ListNode* curr1 = &l1;
+  while (curr1) {
+    std::cout << curr1->val << " ";
+    curr1 = curr1->next;
+  }
+  std::cout << std::endl;
+
+  ListNode* head = sol.insertGreatestCommonDivisors(&l1);
+  while (head) {
+    std::cout << head->val << " ";
+    head = head->next;
   }
 
   return 0;
